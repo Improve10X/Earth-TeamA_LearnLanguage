879053f03312c7b96efd73243c88aa59c2d13d8d
Vinodkumar M <vinod.kumar@improve10x.com>
2022-12-20 16:09:14 +0530

    Added SplashActivity and VideosActivity . completed SplashActivity Xml file.

---
+++ app/src/main/java/com/improve10x/earth_teama_learnlanguage/VideosActivity.java
@@ -0,0 +1,15 @@
+package com.improve10x.earth_teama_learnlanguage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VideosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_videos);
+        getSupportActionBar().setTitle("Videos");
+    }
+}
